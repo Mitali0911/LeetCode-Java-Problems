2fbaf7c56644743a7ae576733d0d919085e90448
Mitali0911 <thakurmitali0911@gmail.com>
2024-09-30 20:39:35 +0530

    Add files via upload

---
+++ Perfect_Number/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 28;
+		 int sum=0;
+	        for(int i=1;i<num;i++){
+	            if(num%i==0){
+	              sum+=i;
+	            }
+	        }
+	        if(sum==num){
+	            System.out.println(sum);
+	        }
+
+	}
+
+}
