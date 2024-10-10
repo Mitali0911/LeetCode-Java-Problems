fd83a694edff8196ca1929ebc5ff4cda4f0bcbae
Mitali0911 <thakurmitali0911@gmail.com>
2024-10-10 21:44:47 +0530

    Add files via upload

---
+++ Powe_Of_Two/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        int n = 16;
+		        for (int i = 0; i < 31; i++) {
+		            int ans = (int) Math.pow(2, i);
+		            if (ans == n) {
+		              System.out.println("yes");
+		            }
+		            else {
+		            	System.out.println("No");
+		            }
+		        }
+		       
+		    }
+		
+	}
+
+
