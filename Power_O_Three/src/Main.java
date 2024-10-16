8ebeb8159b5080227f88b7c0593f5da449e3eefc
Mitali0911 <thakurmitali0911@gmail.com>
2024-10-16 20:44:31 +0530

    Add files via upload

---
+++ Power_O_Three/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n = 27;
+		 if(n==1){
+	            System.out.println("true");;
+	        }
+	        if(n==0 || n%3!=0){
+	            System.out.println("false");
+	        }
+	        System.out.println(n/3);;
+
+	}
+
+}
