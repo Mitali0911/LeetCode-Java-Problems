84fa25d2861576e27748881692f821050a087f52
Mitali0911 <thakurmitali0911@gmail.com>
2024-10-17 21:22:20 +0530

    Add files via upload

---
+++ Power_Of_Four/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n = 16;
+		 if (n == 1) {
+			 System.out.println(1);
+		 }
+	        if (n <= 0 || n % 4 != 0) {
+	        	
+	        }
+	        System.out.println(n/4);
+
+	}
+
+}
